ca56c8ff54202a0a737f842d98ef0acc0c78d806
SimenPus <Simen.brock.stoa@gmail.com>
2024-12-06 15:04:30 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,57 @@
+
+//Oppgave 1
+
+for (let i = 1; i <= 10; i++) {
+    console.log("Hello World");
+}
+
+
+
+// oppgave 2
+// for (let i = 0; i<10; i++) {
+//     document.writeln(`<br>${i}<br>`)
+// }
+
+// //opgave 3
+// var sum = 0;
+// for (let i = 0; i<10; i++) {
+// sum + sum +i;
+// }
+
+// //oppgave 4
+// let output = "";
+// for (let i = 1; i <= 4; i++) { 
+//     output += "#";
+//     console.log(output); 
+    
+// }
+
+// //Oppgave 5
+// for (let i = 2; i<=100; i = i + 2;) {
+//     console.log(i);
+    
+// }
+
+
+//Oppgave 6
+// for (let i = 0; i<= 100; i+= 5) {
+//     document.writeln(`<br>${i}<br>`)
+// } 
+
+//Oppgave 7
+
+// let liste = [1, 1, 22, 123, 3]
+//           // -- --  --  ---  -
+// let sE = liste[0]
+ 
+// for (let i = 0; i < liste.length; i++) {
+//     if (liste[i] > liste[i+1]) {
+//         sE = liste[i]
+//     }
+// }
+ 
+// document.write(`${sE}`);
+
+//Oppgave 8
+
+
